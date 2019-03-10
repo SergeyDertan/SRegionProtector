6953bdea274324beb7af7cc8108ad4c74a6d6d45
SergeyDertan <superator2018@gmail.com>
2019-03-10 20:51:38 +0200

    Improve LPos commands

--- src/main/java/Sergey_Dertan/SRegionProtector/Command/Creation/LPos2Command.java
+++ src/main/java/Sergey_Dertan/SRegionProtector/Command/Creation/LPos2Command.java
@@ -5,7 +5,6 @@ import Sergey_Dertan.SRegionProtector.Region.Selector.RegionSelector;
 import cn.nukkit.Player;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.level.Position;
-import cn.nukkit.math.Vector3;
 import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
 
 public final class LPos2Command extends SRegionProtectorCommand {
@@ -32,17 +31,14 @@ public final class LPos2Command extends SRegionProtectorCommand {
             this.messenger.sendMessage(sender, "command.lpos2.in-game");
             return false;
         }
-        double x = -Math.sin(((Player) sender).yaw / 180 * Math.PI) * Math.cos(((Player) sender).pitch / 180 * Math.PI);
-        double y = -Math.sin(((Player) sender).pitch / 180 * Math.PI);
-        double z = Math.cos(((Player) sender).yaw / 180 * Math.PI) * Math.cos(((Player) sender).pitch / 180 * Math.PI);
-        Vector3 pos = new Vector3(((Player) sender).x, ((Player) sender).y + ((Player) sender).getEyeHeight(), ((Player) sender).z);
-        while (pos.distance(((Vector3) sender)) < this.maxRadius) {
-            if (((Player) sender).level.getBlock(pos = pos.add(x, y, z)).getId() == 0) continue;
-            this.selector.getSession((Player) sender).pos2 = new Position(Math.floor(pos.x), Math.floor(pos.y), Math.floor(pos.z), ((Player) sender).level);
+        Position pos = ((Player) sender).getTargetBlock(this.maxRadius);
+        if (pos != null) {
+            this.selector.getSession((Player) sender).pos2 = Position.fromObject(pos, pos.level);
             this.messenger.sendMessage(sender, "command.lpos2.success");
             return true;
+        } else {
+            this.messenger.sendMessage(sender, "command.lpos2.fail", "@radius", Integer.toString(this.maxRadius));
+            return false;
         }
-        this.messenger.sendMessage(sender, "command.lpos2.fail", "@radius", Integer.toString(this.maxRadius));
-        return false;
     }
 }
