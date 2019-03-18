0f7b04aed3757eb4a5ccbbf59f4beb3799e11f2d
SergeyDertan <superator2018@gmail.com>
2019-03-18 23:35:56 +0200

    Remove unused tag

--- src/main/java/Sergey_Dertan/SRegionProtector/Utils/Tags.java
+++ src/main/java/Sergey_Dertan/SRegionProtector/Utils/Tags.java
@@ -1,6 +1,7 @@
 package Sergey_Dertan.SRegionProtector.Utils;
 
 public abstract class Tags { //do I need this?
+
     public final static String MIN_X_TAG = "min_x";
     public final static String MIN_Y_TAG = "min_y";
     public final static String MIN_Z_TAG = "min_z";
@@ -15,7 +16,6 @@ public abstract class Tags { //do I need this?
     public final static String NAME_TAG = "name";
     public final static String LEVEL_TAG = "level";
     public final static String CREATOR_TAG = "creator";
-    public final static String REGIONS_TAG = "regions";
     public final static String STATE_TAG = "state";
     public final static String PRICE_TAG = "price";
     public final static String POSITION_TAG = "position";
