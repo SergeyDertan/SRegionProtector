c95198449ef78aea2168a81384566bffdffb3c16
Sergey Dertan <superator2018@gmail.com>
2019-04-27 01:56:37 +0300

    Create maven repository for fastutil-lite

--- src/main/java/Sergey_Dertan/SRegionProtector/Settings/RegionSettings.java
+++ src/main/java/Sergey_Dertan/SRegionProtector/Settings/RegionSettings.java
@@ -19,7 +19,7 @@ import static Sergey_Dertan.SRegionProtector.Region.Flags.RegionFlags.FLAG_AMOUN
 public final class RegionSettings {
 
     public final boolean[] flagsStatus = new boolean[FLAG_AMOUNT];
-    public final boolean[] defaultFlags = new boolean[FLAG_AMOUNT];
+    public final boolean[] defaultFlags = new boolean[FLAG_AMOUNT]; //default flag value while creating region
     public final boolean[] needMessage = new boolean[FLAG_AMOUNT]; //check if player will see the message
 
     public final boolean[] display = new boolean[FLAG_AMOUNT]; //check if flag should be shown in the info command
