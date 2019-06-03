8532c29e4a89527067ee8d2baa546c61fc744dd7
SergeyDertan <superator2018@gmail.com>
2019-06-03 22:03:55 +0300

    Fix form UI

--- src/main/java/Sergey_Dertan/SRegionProtector/UI/Form/Type/UIForm.java
+++ src/main/java/Sergey_Dertan/SRegionProtector/UI/Form/Type/UIForm.java
@@ -4,6 +4,7 @@ import Sergey_Dertan.SRegionProtector.Region.Region;
 import cn.nukkit.Player;
 import cn.nukkit.form.response.FormResponse;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,19 +13,21 @@ public interface UIForm {
     Class<? extends UIForm> MAIN = MainForm.class;
 
     static UIForm getInstance(Class<? extends UIForm> clazz, Object... args) {
-        try {
-            List<Class<?>> parametersTypes = new ArrayList<>();
-            for (Object obj : args) {
-                if (obj instanceof Player) {
-                    parametersTypes.add(Player.class);
-                    continue;
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+            List<Class<?>> parameters = new ArrayList<>();
+            if (constructor.getParameterCount() != args.length) continue;
+            int i = -1;
+            for (Class<?> param : constructor.getParameterTypes()) {
+                if (param.isAssignableFrom(args[++i].getClass())) {
+                    parameters.add(param);
                 }
-                parametersTypes.add(obj.getClass());
             }
-            return clazz.getDeclaredConstructor(parametersTypes.toArray(new Class<?>[0])).newInstance(args);
-        } catch (Exception e) {
-            return null;
+            try {
+                return clazz.getDeclaredConstructor(parameters.toArray(new Class<?>[0])).newInstance(args);
+            } catch (Exception ignore) {
+            }
         }
+        return null;
     }
 
     Region getRegion();
