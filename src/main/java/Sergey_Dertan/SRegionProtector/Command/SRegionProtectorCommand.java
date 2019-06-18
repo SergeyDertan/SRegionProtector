13ff983034b0b066d3a17415409813d19b9633a0
SergeyDertan <superator2018@gmail.com>
2019-06-18 17:23:40 +0300

    PluginIdentifiableCommand

--- src/main/java/Sergey_Dertan/SRegionProtector/Command/SRegionProtectorCommand.java
+++ src/main/java/Sergey_Dertan/SRegionProtector/Command/SRegionProtectorCommand.java
@@ -1,9 +1,11 @@
 package Sergey_Dertan.SRegionProtector.Command;
 
+import Sergey_Dertan.SRegionProtector.Main.SRegionProtectorMain;
 import Sergey_Dertan.SRegionProtector.Messenger.Messenger;
 import cn.nukkit.command.Command;
+import cn.nukkit.command.PluginIdentifiableCommand;
 
-public abstract class SRegionProtectorCommand extends Command {
+public abstract class SRegionProtectorCommand extends Command implements PluginIdentifiableCommand {
 
     protected final Messenger messenger;
 
@@ -21,5 +23,10 @@ public abstract class SRegionProtectorCommand extends Command {
 
     public SRegionProtectorCommand(String name, String mp) {
         this(name, mp, mp);
+    }
+
+    @Override
+    public SRegionProtectorMain getPlugin() {
+        return SRegionProtectorMain.getInstance();
     }
 }
